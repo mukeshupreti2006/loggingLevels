5ffa46a5d8dc66ef6987ce48a6fc620c5c5f4e8e
mukesh.upreti <mukeshupreti2006@gmail.com>
2019-08-24 15:22:38 +0530

    Initial commit

---
+++ src/main/java/com/learning/logging/loggingLevels/LoggingLevelsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.logging.loggingLevels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingLevelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingLevelsApplication.class, args);
+	}
+
+}
